5824978463c37c290bead1fd5e56ace15924f9fb
Nannapas1997 <nannapas4869@gmail.com>
2024-12-31 06:30:08 +0700

    edit employee 1

--- src/pages/Employee_2.jsx
+++ src/pages/Employee_2.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
-import Select from "react-select";
 import SearchBar from "../components/SearchBar";
+import Select from "react-select";
+import DataTable from "../components/DataTable";
+import "remixicon/fonts/remixicon.css";
 import Switch from "../components/Switch";
+import Modal from "react-modal";
+import Kristi from "../assets/images/Kristi.png"
+import Call from "../assets/images/Call.png"
+import Mail from "../assets/images/Mail.png"
+import Address from "../assets/images/address.png"
 function Employee_2() {
   const [selectedDepartment, setSelectedDepartment] = useState("All");
-
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
   const headers = [
     "No.",
     "Employee Name",
@@ -45,7 +52,10 @@ function Employee_2() {
       </div>,
     ],
   ];
-
+  const options = [
+    { value: "Active", label: "Active" },
+    { value: "Inactive", label: "Inactive" },
+  ];
   const options2 = [
     { value: "All", label: "All Department" },
     { value: "Marketing", label: "Marketing" },
@@ -83,6 +93,14 @@ function Employee_2() {
               options={options2}
               placeholder="All Department"
               onChange={handleDepartmentChange}
+            />
+             <Select
+              className="basic-single w-44 mt-10 mb-10"
+              classNamePrefix="select"
+              defaultValue={""}
+              name="status"
+              options={options}
+              placeholder="All Status"
             />
           </div>
         </div>
@@ -128,6 +146,56 @@ function Employee_2() {
             </tbody>
           </table>
         </div>
+        {isModalOpen2 && (
+        <div
+          className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50 transition-opacity duration-300"
+          onClick={() => setIsModalOpen2(false)}
+        >
+          <div
+            className="bg-white p-6 rounded shadow-lg w-6/12 transition-transform transform scale-100"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <h2 className="text-3xl font-semibold mb-4">View Employee</h2>
+            <hr className="mt-5 mb-5"/>
+            <div className="flex flex-row justify-start gap-3">
+                <div>
+                    <img src={Kristi} alt="Kristi" width="100%" />
+                </div>
+                
+                <div className="flex flex-col ">
+                    <p className="text-xl">Kristi Christiansen</p>
+                    <p className="text-[#6C6C6C] font-light">Full Stack Developer (IT)</p>
+                </div>
+            </div>
+            <div className="flex flex-row justify-start gap-3 mt-7">
+                <div>
+                    <img src={Call} alt="call" />
+                </div>
+                <p className="text-[#6C6C6C] font-light text-xl padding_0">(+66) 67 4759 548</p>
+            </div>
+            <div className="flex flex-row justify-start gap-3 mt-7">
+                <div>
+                    <img src={Mail} alt="mail" className="mt-1" />
+                </div>
+                <p className="text-[#6C6C6C] font-light text-xl padding_0">Kristi_Christiansen@gmail.com</p>
+            </div>
+            <div className="flex flex-row justify-start gap-3 mt-7">
+                <div>
+                    <img src={Address} alt="address" className="mt-1" />
+                </div>
+                <p className="text-[#6C6C6C] font-light text-xl padding_0">73958 Larch Close, New Norbert 35047</p>
+            </div>
+            <div className="flex justify-end aligns-end">
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 mr-4"
+                onClick={() => setIsModalOpen2(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       </div>
     </article>
   );
